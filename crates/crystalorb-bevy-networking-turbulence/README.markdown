692cbb04dacc06c18c559dd363f202028a5fd056
Ernest Wong <sudo@ernestwong.nz>
2021-06-08 22:32:07 +1200

    Implement bevy plugin over bevy_networking_turbulence

--- crates/crystalorb-bevy-networking-turbulence/README.markdown
+++ crates/crystalorb-bevy-networking-turbulence/README.markdown
@@ -1,3 +1,155 @@
 # crystalorb-bevy-networking-turbulence
 
 Provides a wrapper implementation of `crystalorb::network_recource::NetworkResource` that allows the `bevy_networking_turbulence` plugin to be used with [`CrystalOrb`](https://github.com/ErnWong/crystalorb).
+
+For your convenience, a bevy plugin is also provided that performs the necessary setup (such as registering the required message channels in the `bevy_networking_turbulence` plugin, and registering the `Client`/`Server` resources)
+
+## Pitfalls
+
+At the moment, you still need to put all your physics and real-time game logic into the `CrystalOrb::world::World`, but, ideally, it would be great to put them into the bevy ECS instead (since the ECS is probably the reason why you want to use bevy in the first place). In the future, it might be possible to implement `CrystalOrb::world::World` for a bevy app (contributions welcome!!) so that we can run bevy inside CrystalOrb inside bevy (bevyception? That'll be a cool name if it's not already taken).
+
+## Usage
+
+- Add the `CrystalOrbClientPlugin` bevy [plugin](https://bevyengine.org/learn/getting-started/plugins/) into your client bevy app.
+- Add the `CrystalOrbServerPlugin` bevy [plugin](https://bevyengine.org/learn/getting-started/plugins/) into your server bevy app.
+- Access the `CrystalOrb::Client` on your client app as a bevy [resource](https://bevyengine.org/learn/getting-started/resources/).
+- Access the `CrystalOrb::Server` on your server app as a bevy [resource](https://bevyengine.org/learn/getting-started/resources/).
+
+Heres an example client:
+
+```rust
+use bevy::prelude::*;
+use crystalorb_demo::{DemoWorld, DemoCommand, PlayerSide, PlayerCommand};
+use crystalorb_bevy_networking_turbulence::{
+  WrappedNetworkResource,
+  CrystalOrbClientPlugin,
+  crystalorb::{
+    Config,
+    client::{
+      Client,
+      stage::StageMut as ClientStageMut,
+    },
+  },
+  CommandChannelSettings,
+  bevy_networking_turbulence::{
+    NetworkResource,
+    MessageChannelSettings,
+    MessageChannelMode,
+    ReliableChannelSettings
+  },
+};
+use std::time::Duration;
+
+#[derive(Default)]
+struct PlayerInputState {
+  jump: bool,
+}
+
+fn player_input(
+  mut state: Local<PlayerInputState>,
+  input: Res<Input<KeyCode>>,
+  mut client: ResMut<Client<DemoWorld>>,
+  mut net: ResMut<NetworkResource>,
+) {
+  if let ClientStageMut::Ready(mut ready_client) = client.stage_mut() {
+    let jump = input.pressed(KeyCode::Up);
+
+    if jump != state.jump {
+      ready_client.issue_command(
+        DemoCommand::new(
+          PlayerSide::Left,
+          PlayerCommand::Jump,
+          jump
+        ),
+        &mut WrappedNetworkResource(&mut *net),
+      );
+
+    }
+
+    state.jump = jump;
+  }
+}
+
+fn main() {
+  App::build()
+    // You can optionally override some message channel settings
+    // There is `CommandChannelSettings`, `SnapshotChannelSettings`, and `ClockSyncChannelSettings`
+    // Make sure you apply the same settings for both client and server.
+    .insert_resource(CommandChannelSettings(
+        MessageChannelSettings {
+            channel: 0,
+            channel_mode: MessageChannelMode::Compressed {
+                reliability_settings: ReliableChannelSettings {
+                    bandwidth: 4096,
+                    recv_window_size: 1024,
+                    send_window_size: 1024,
+                    burst_bandwidth: 1024,
+                    init_send: 512,
+                    wakeup_time: Duration::from_millis(100),
+                    initial_rtt: Duration::from_millis(200),
+                    max_rtt: Duration::from_secs(2),
+                    rtt_update_factor: 0.1,
+                    rtt_resend_factor: 1.5,
+                },
+                max_chunk_len: 1024,
+            },
+            message_buffer_size: 64,
+            packet_buffer_size: 64,
+        }
+    ))
+    .add_plugins(DefaultPlugins)
+    .add_plugin(CrystalOrbClientPlugin::<DemoWorld>::new(Config::default()))
+    .add_system(player_input.system())
+    .run();
+}
+```
+
+Here's an example server:
+
+```rust
+use bevy::prelude::*;
+use crystalorb_demo::DemoWorld;
+use crystalorb_bevy_networking_turbulence::{
+  CrystalOrbServerPlugin,
+  crystalorb::Config,
+  CommandChannelSettings,
+  bevy_networking_turbulence::{
+    MessageChannelSettings,
+    MessageChannelMode,
+    ReliableChannelSettings
+  },
+};
+use std::time::Duration;
+
+fn main() {
+  App::build()
+    // You can optionally override some message channel settings
+    // There is `CommandChannelSettings`, `SnapshotChannelSettings`, and `ClockSyncChannelSettings`
+    // Make sure you apply the same settings for both client and server.
+    .insert_resource(CommandChannelSettings(
+        MessageChannelSettings {
+            channel: 0,
+            channel_mode: MessageChannelMode::Compressed {
+                reliability_settings: ReliableChannelSettings {
+                    bandwidth: 4096,
+                    recv_window_size: 1024,
+                    send_window_size: 1024,
+                    burst_bandwidth: 1024,
+                    init_send: 512,
+                    wakeup_time: Duration::from_millis(100),
+                    initial_rtt: Duration::from_millis(200),
+                    max_rtt: Duration::from_secs(2),
+                    rtt_update_factor: 0.1,
+                    rtt_resend_factor: 1.5,
+                },
+                max_chunk_len: 1024,
+            },
+            message_buffer_size: 64,
+            packet_buffer_size: 64,
+        }
+    ))
+    .add_plugins(DefaultPlugins)
+    .add_plugin(CrystalOrbServerPlugin::<DemoWorld>::new(Config::default()))
+    .run();
+}
+```
--- crates/crystalorb-bevy-networking-turbulence/src/lib.rs
+++ crates/crystalorb-bevy-networking-turbulence/src/lib.rs
@@ -2,13 +2,31 @@
 #![feature(extended_key_value_attributes)]
 #![doc = include_str!("../README.markdown")]
 
-use bevy_networking_turbulence::{ConnectionHandle, NetworkResource};
-use crystalorb::network_resource::{
-    Connection as ConnectionTrait, ConnectionHandleType, NetworkResource as NetworkResourceTrait,
+use bevy_app::{AppBuilder, EventReader, EventWriter, Plugin};
+use bevy_core::Time;
+use bevy_ecs::prelude::*;
+use bevy_networking_turbulence::{
+    ConnectionChannelsBuilder, ConnectionHandle, MessageChannelMode, MessageChannelSettings,
+    NetworkEvent, NetworkResource, NetworkingPlugin, ReliableChannelSettings,
+};
+use crystalorb::{
+    client::{stage::Stage as ClientStage, Client},
+    clocksync::ClockSyncMessage,
+    network_resource::{
+        Connection as ConnectionTrait, ConnectionHandleType,
+        NetworkResource as NetworkResourceTrait,
+    },
+    server::Server,
+    timestamp::Timestamped,
+    world::World,
+    Config,
 };
 use serde::{de::DeserializeOwned, Serialize};
-use std::fmt::Debug;
+use std::{fmt::Debug, marker::PhantomData, time::Duration};
 use turbulence::MessageChannels;
+
+pub use bevy_networking_turbulence;
+pub use crystalorb;
 
 pub struct WrappedNetworkResource<'a>(pub &'a mut NetworkResource);
 pub struct WrappedConnection<'a>(pub &'a mut MessageChannels);
@@ -55,5 +73,247 @@ impl<'a> ConnectionTrait for WrappedConnection<'a> {
         MessageType: Debug + Clone + Serialize + DeserializeOwned + Send + Sync + 'static,
     {
         self.0.flush::<MessageType>()
+    }
+}
+
+pub struct CommandChannelSettings(pub MessageChannelSettings);
+pub struct SnapshotChannelSettings(pub MessageChannelSettings);
+pub struct ClockSyncChannelSettings(pub MessageChannelSettings);
+
+trait MyClone {
+    fn clone(&self) -> Self;
+}
+
+impl MyClone for MessageChannelMode {
+    fn clone(&self) -> Self {
+        match self {
+            MessageChannelMode::Unreliable => MessageChannelMode::Unreliable,
+            MessageChannelMode::Reliable {
+                reliability_settings,
+                max_message_len,
+            } => MessageChannelMode::Reliable {
+                reliability_settings: reliability_settings.clone(),
+                max_message_len: *max_message_len,
+            },
+            MessageChannelMode::Compressed {
+                reliability_settings,
+                max_chunk_len,
+            } => MessageChannelMode::Compressed {
+                reliability_settings: reliability_settings.clone(),
+                max_chunk_len: *max_chunk_len,
+            },
+        }
+    }
+}
+
+impl MyClone for MessageChannelSettings {
+    fn clone(&self) -> Self {
+        Self {
+            channel: self.channel,
+            channel_mode: self.channel_mode.clone(),
+            message_buffer_size: self.message_buffer_size,
+            packet_buffer_size: self.packet_buffer_size,
+        }
+    }
+}
+
+impl Default for CommandChannelSettings {
+    fn default() -> Self {
+        Self(MessageChannelSettings {
+            channel: 0,
+            channel_mode: MessageChannelMode::Compressed {
+                reliability_settings: ReliableChannelSettings {
+                    bandwidth: 4096,
+                    recv_window_size: 1024,
+                    send_window_size: 1024,
+                    burst_bandwidth: 1024,
+                    init_send: 512,
+                    wakeup_time: Duration::from_millis(100),
+                    initial_rtt: Duration::from_millis(200),
+                    max_rtt: Duration::from_secs(2),
+                    rtt_update_factor: 0.1,
+                    rtt_resend_factor: 1.5,
+                },
+                max_chunk_len: 1024,
+            },
+            message_buffer_size: 64,
+            packet_buffer_size: 64,
+        })
+    }
+}
+
+impl Clone for CommandChannelSettings {
+    fn clone(&self) -> Self {
+        Self(self.0.clone())
+    }
+}
+
+impl Default for SnapshotChannelSettings {
+    fn default() -> Self {
+        Self(MessageChannelSettings {
+            channel: 1,
+            channel_mode: MessageChannelMode::Unreliable,
+            message_buffer_size: 64,
+            packet_buffer_size: 64,
+        })
+    }
+}
+
+impl Clone for SnapshotChannelSettings {
+    fn clone(&self) -> Self {
+        Self(self.0.clone())
+    }
+}
+
+impl Default for ClockSyncChannelSettings {
+    fn default() -> Self {
+        Self(MessageChannelSettings {
+            channel: 2,
+            channel_mode: MessageChannelMode::Unreliable,
+            message_buffer_size: 64,
+            packet_buffer_size: 64,
+        })
+    }
+}
+
+impl Clone for ClockSyncChannelSettings {
+    fn clone(&self) -> Self {
+        Self(self.0.clone())
+    }
+}
+
+pub fn network_setup<WorldType: World>(
+    mut net: ResMut<NetworkResource>,
+    command_channel_settings_res: Res<CommandChannelSettings>,
+    snapshot_channel_settings_res: Res<CommandChannelSettings>,
+    clocksync_channel_settings_res: Res<ClockSyncChannelSettings>,
+) {
+    // Ugly consequence of set_channels_builder not accepting a FnOnce.
+    let command_channel_settings = command_channel_settings_res.clone();
+    let snapshot_channel_settings = snapshot_channel_settings_res.clone();
+    let clocksync_channel_settings = clocksync_channel_settings_res.clone();
+
+    net.set_channels_builder(move |builder: &mut ConnectionChannelsBuilder| {
+        builder
+            .register::<Timestamped<WorldType::CommandType>>(command_channel_settings.clone().0)
+            .unwrap();
+
+        builder
+            .register::<Timestamped<WorldType::SnapshotType>>(snapshot_channel_settings.clone().0)
+            .unwrap();
+
+        builder
+            .register::<ClockSyncMessage>(clocksync_channel_settings.clone().0)
+            .unwrap();
+    });
+}
+
+pub fn server_system<WorldType: World>(
+    mut server: ResMut<Server<WorldType>>,
+    time: Res<Time>,
+    mut net: ResMut<NetworkResource>,
+) {
+    server.update(
+        time.delta_seconds_f64(),
+        time.seconds_since_startup(),
+        &mut WrappedNetworkResource(&mut *net),
+    );
+}
+
+pub struct CrystalOrbServerPlugin<WorldType: World> {
+    config: Config,
+    _world: PhantomData<WorldType>,
+}
+
+impl<WorldType: World> CrystalOrbServerPlugin<WorldType> {
+    pub fn new(config: Config) -> Self {
+        Self {
+            config,
+            _world: PhantomData,
+        }
+    }
+}
+
+impl<WorldType: World> Plugin for CrystalOrbServerPlugin<WorldType> {
+    fn build(&self, app: &mut AppBuilder) {
+        app.add_plugin(NetworkingPlugin::default())
+            .insert_resource(Server::<WorldType>::new(self.config.clone(), 0.0))
+            .init_resource::<CommandChannelSettings>()
+            .init_resource::<SnapshotChannelSettings>()
+            .init_resource::<ClockSyncChannelSettings>()
+            .add_startup_system(network_setup::<WorldType>.system())
+            .add_system(server_system::<WorldType>.system());
+    }
+}
+
+#[derive(Debug)]
+pub enum ClientConnectionEvent {
+    Connected(usize),
+    Disconnected(usize),
+}
+
+#[derive(Default)]
+pub struct ClientSystemState {
+    is_ready: bool,
+}
+
+pub fn client_system<WorldType: World>(
+    mut client_system_state: Local<ClientSystemState>,
+    mut network_event_reader: EventReader<NetworkEvent>,
+    mut client: ResMut<Client<WorldType>>,
+    time: Res<Time>,
+    mut net: ResMut<NetworkResource>,
+    mut client_connection_events: EventWriter<ClientConnectionEvent>,
+) {
+    // TODO: For now, disconnection events are fatal and we do not attempt to reconnect. This
+    // is why it is handled specially, in this location, rather than as a ClientState
+    // transition.
+    for network_event in network_event_reader.iter() {
+        if let NetworkEvent::Disconnected(handle) = network_event {
+            client_connection_events.send(ClientConnectionEvent::Disconnected(*handle as usize));
+        }
+    }
+    client.update(
+        time.delta_seconds_f64(),
+        time.seconds_since_startup(),
+        &mut WrappedNetworkResource(&mut *net),
+    );
+    match client.stage() {
+        ClientStage::Ready(client) => {
+            if !client_system_state.is_ready {
+                client_connection_events.send(ClientConnectionEvent::Connected(client.client_id()));
+            }
+            client_system_state.is_ready = true;
+        }
+        _ => {
+            client_system_state.is_ready = false;
+        }
+    }
+}
+
+pub struct CrystalOrbClientPlugin<WorldType: World> {
+    config: Config,
+    _world: PhantomData<WorldType>,
+}
+
+impl<WorldType: World> CrystalOrbClientPlugin<WorldType> {
+    pub fn new(config: Config) -> Self {
+        Self {
+            config,
+            _world: PhantomData,
+        }
+    }
+}
+
+impl<WorldType: World> Plugin for CrystalOrbClientPlugin<WorldType> {
+    fn build(&self, app: &mut AppBuilder) {
+        app.add_plugin(NetworkingPlugin::default())
+            .insert_resource(Client::<WorldType>::new(self.config.clone()))
+            .init_resource::<CommandChannelSettings>()
+            .init_resource::<SnapshotChannelSettings>()
+            .init_resource::<ClockSyncChannelSettings>()
+            .add_event::<ClientConnectionEvent>()
+            .add_startup_system(network_setup::<WorldType>.system())
+            .add_system(client_system::<WorldType>.system());
     }
 }
