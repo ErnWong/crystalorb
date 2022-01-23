5f318b4971fb9123093cd18289046c17c1a75f9b
Ernest Wong <sudo@ernestwong.nz>
2022-01-23 13:24:31 +1300

    Add basic nix flake direnv to load rustup into development environment

---
+++ flake.nix
@@ -0,0 +1,20 @@
+{
+  description = "CrystalOrb";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs {
+        inherit system;
+        config.allowUnfree = true;
+      };
+    in {
+      devShell = pkgs.mkShell {
+        nativeBuildInputs = with pkgs; [
+          rustup
+        ];
+      };
+    });
+}
